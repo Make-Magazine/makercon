fb2f6b8bd5e5538458acabecc0d60c1eba0eda45
Bill Olson <bolson@makermedia.com>
2014-02-11 14:07:59 -0800

    Adding less styles

--- header.php
+++ header.php
@@ -15,6 +15,9 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<script type="text/javascript" src="//use.typekit.net/rya4lcn.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
 <?php wp_head(); ?>
 </head>
 
