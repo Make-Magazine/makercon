9fc782d99f3713eb1610f49d040ae85905630ca3
Jake Spurlock <jspurlock@makermedia.com>
2014-02-13 16:39:51 -0800

    Drop the footer stuff.

--- footer.php
+++ footer.php
@@ -20,13 +20,12 @@
 				<p>Maker Pro Newsletter is a weekly roundup of news about the impact makers are having on business and technology. From new hardware products and devices, hot startups, incubators and innovators, to new tools and market trends, Maker Pro delivers a quick read to keep you up to speed on the business of making. <a href="#">Subscribe today!</a></p>
 			</div>
 			<div class="col-md-4">
-				<!-- <h3>Venue</h3>
+				<h3>Venue</h3>
 				<address>
-					Building Name<br>
-					1256 Civic St., Room 15<br>
-					San Grancisco, CA 95437
+					Redwood City, CA<br>
+					May 13th - 14th 2014
 				</address>
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/map.jpg" alt="Venue Map"> -->
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/map.jpg" alt="Venue Map">
 			</div>
 		</div>
 		<div class="site-info">
