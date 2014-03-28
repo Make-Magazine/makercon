a10141de484cdc0d09c4f6cbf1d3349427f2dccd
Jake Spurlock <whyisjake@gmail.com>
2014-03-28 15:58:47 -0700

    Bill's update for sponsors

--- content-home.php
+++ content-home.php
@@ -8,6 +8,30 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('home'); ?>>
 
+		<div class="white-box" style="padding-top:30px !important;padding-bottom:30px !important">
+		<div class="row">
+		<div class="row sponsors-home">
+		    <div class="col-md-4">
+		    	<h3>Supporting Sponsor</h3>
+		    	<a href="http://www.shopbottools.com/">
+		    		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/shopbot-sponsor-sidebar.jpg" alt="Shopbot Logo" width="200px" style="margin-top:25px;" />
+			</a>
+		    </div>
+		    <div class="col-md-4">
+				<h3>Host Sponsor</h3>
+				<a href="http://www.oracle.com">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Oracle-Java_Logo_wide.gif" alt="Oracle Logo" width="220px">
+				</a>
+		    </div>
+		    <div class="col-md-4">
+				<h3>Media Partner</h3>
+				<a href="http://www.marketwire.com/">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/marketwired_logo.jpg" alt="Market Wire Logo" width="200" style="margin-top:15px;" />
+				</a>
+		    </div>
+		</div>
+	</div>
+
 	<div class="entry-content">
 		<div class="row">
 			<div class="col-md-6">
@@ -42,15 +66,6 @@
 		<div class="row">
 			<div class="col-md-12">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/red-banner.jpg" alt="A leader of the maker movement, MAKE has been integral in helping DIY hobbyists avail themselves of the tools and technologies—such as 3D printers, CNC mills, and microcontrollers—to design and engineer innovative products and devices.">
-			</div>
-		</div>
-	</div>
-
-	<div class="white-box">
-		<div class="row">
-			<div class="col-md-12">
-				<h2>Sponsorships:</h2>
-				<p>Become a MakerCon sponsor! Connect your brand to the growing community of makers having an exciting and enduring impact on our world! We have created a number of sponsorship opportunities for companies, media organizations, public institutions and trade associations. Contact: sponsors@makerconference.com for more information.</p>
 			</div>
 		</div>
 	</div>
