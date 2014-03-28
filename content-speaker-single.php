e2a9609a1c37ae1dee16d6f5221beef9032535b0
Jake Spurlock <whyisjake@gmail.com>
2014-03-28 16:43:48 -0700

    Why did this ever work?

--- content-speaker-single.php
+++ content-speaker-single.php
@@ -19,11 +19,11 @@
 				<?php if ( has_post_thumbnail() ) {
 					the_post_thumbnail( 'medium', array( 'class' => 'img-responsive' ) );
 				} else {
-					echo Gravity_Forms_Helper::make_image( Gravity_Forms_Helper::get_entry( array( 'form' => $selected_speaker, 'field' => 25,  ) ), '300', get_the_title() );
+					echo Gravity_Forms_Helper::make_image( Gravity_Forms_Helper::get_entry( array( 'entry' => $selected_speaker, 'field' => 25,  ) ), '300', get_the_title() );
 				} ?>
 			</div>
 			<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-				<?php echo ( get_the_content() ) ? the_content() : apply_filters( 'the_content', Gravity_Forms_Helper::get_entry( array( 'form' => 1, 'field' => 6 ) ) ); ?>
+				<?php echo ( get_the_content() ) ? the_content() : apply_filters( 'the_content', Gravity_Forms_Helper::get_entry( array( 'entry' => $selected_speaker, 'field' => 6 ) ) ); ?>
 			</div>
 		</div>
 
