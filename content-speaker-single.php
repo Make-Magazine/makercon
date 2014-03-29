f46cd6b9db6d124535e999f74000587d04d20958
Jake Spurlock <jspurlock@makermedia.com>
2014-03-29 13:28:22 -0700

    Call the speaker loop function instead of outputting on the page.

--- content-speaker-single.php
+++ content-speaker-single.php
@@ -12,22 +12,8 @@
 				<h1 class="entry-title"><?php the_title(); ?></h1>
 			</a>
 		</div>
-		<?php $selected_speaker = get_post_meta( get_the_id(), 'selected_speaker', true ); ?>
-		<!-- Let's setup a row of stuff... -->
-		<div class="row">
-			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-				<?php if ( has_post_thumbnail() ) {
-					the_post_thumbnail( 'medium', array( 'class' => 'img-responsive' ) );
-				} else {
-					echo Gravity_Forms_Helper::make_image( Gravity_Forms_Helper::get_entry( array( 'entry' => $selected_speaker, 'field' => 25,  ) ), '300', get_the_title() );
-				} ?>
-			</div>
-			<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-				<?php echo ( get_the_content() ) ? the_content() : apply_filters( 'the_content', Gravity_Forms_Helper::get_entry( array( 'entry' => $selected_speaker, 'field' => 6 ) ) ); ?>
-			</div>
-		</div>
 
-		<hr>
+		<?php echo Speaker_Meta::speaker_loop(); ?>
 
 		<?php
 			wp_link_pages( array(
