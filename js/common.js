89da7c6900d51eb68730ce6c48cec529172f04e8
Jake Spurlock <whyisjake@gmail.com>
2014-04-24 10:22:01 -0700

    Bill's updates

---
+++ js/common.js
@@ -0,0 +1,18 @@
+jQuery( document ).ready( function( $ ) {
+    // Allow links within bootstrap tabs for sharing URL of a particular tab
+    var url = document.location.toString();
+    if ( url.match( '#' ) ) {
+        $( '.nav-tabs a[href=#' + url.split( '#' )[1] + ']').tab( 'show' ) ;
+    }
+
+    // Change hash in URL for tab linking
+    $( '.nav-tabs a' ).on( 'shown', function( e ) {
+        // Use the History API if possible, or else, fall back.
+        // The History API will allow us to hash a URL without page jump in modern browsers.
+        if ( history.pushState ) {
+            window.history.pushState( null, null, e.target.hash );
+        } else {
+            window.location.hash = e.target.hash;
+        }
+    });
+});
