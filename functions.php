b3e6b8c7034fe56d079dba3a85947cee7f0e3484
Jake Spurlock <jspurlock@makermedia.com>
2014-05-08 14:21:21 -0700

    Adding Google Analytics tracking

--- functions.php
+++ functions.php
@@ -251,3 +251,21 @@ if( function_exists( "register_field_group" ) ) {
 		'menu_order' => 0,
 	));
 }
+
+
+function add_google_analytics() { ?>
+
+	<script>
+
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-51157-24', 'makercon.com');
+		ga('send', 'pageview');
+
+	</script>
+<?php }
+
+add_filter('wp_head', 'add_google_analytics');
