2e150ab206e75a2700407319c86f9ec77f57f80c
Jake Spurlock <jspurlock@makermedia.com>
2014-05-23 11:43:25 -0700

    Why not write some Javascript today?

--- js/navigation.js
+++ js/navigation.js
@@ -32,3 +32,44 @@
 			container.className += ' toggled';
 	};
 } )();
+
+jQuery( document ).ready( function( $ ) {
+	var url = 'https://public-api.wordpress.com/rest/v1/sites/makezine.com/posts/?tag=makercon&number=2';
+	var jqxhr = $.getJSON( url, function() {
+		var html_output = '<h2>News from MakerCon</h2><ul class="recent-posts media-list">';
+		$( jqxhr.responseJSON.posts ).each( function() {
+			html_output += '<li class="media">';
+			html_output += '<a class="pull-left" href="' + $(this)[0].URL + '"><img class="media-object thumbnail" src="' + $(this)[0].featured_image + '?w=125" alt="' + $(this)[0].title + '"></a>'
+			html_output += '<h3><a href="' + $(this)[0].URL + '">' + $(this)[0].title + '</a></h3>';
+			html_output += $(this)[0].excerpt;
+			html_output += '</li>';
+		});
+		html_output += '</ul>';
+		$('.highlights .posts').html( html_output );
+	})
+
+	var slides = '';
+	var i = 0;
+	$( images ).each( function() {
+		if ( i == 0 ) {
+			slides += '<div class="item active"><img src="' + $(this)[0].path + '?w=520"></div>';
+		} else {
+			slides += '<div class="item"><img src="' + $(this)[0].path + '?w=520"></div>';
+		}
+		i++;
+	});
+	$('.home-slideshow').html( slides );
+
+	var indicators = '';
+	var i = 0;
+	$( images ).each( function() {
+		if ( i == 0 ) {
+			indicators += '<li data-target="#carousel-example-generic" data-slide-to="' + i + '" class="active"></li>';
+		} else {
+			indicators += '<li data-target="#carousel-example-generic" data-slide-to="' + i + '"></li>';
+		}
+		i++;
+	});
+	$('.carousel-indicators').html( indicators );
+
+});
