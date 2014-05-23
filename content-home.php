25fa2a8b5e05a9ded1cf2959290206447f939529
Jake Spurlock <jspurlock@makermedia.com>
2014-05-23 11:42:38 -0700

    Slideshow.

--- content-home.php
+++ content-home.php
@@ -69,6 +69,112 @@
 				</ul>
 			</div>
 		</div>
+		<div class="row highlights">
+			<div class="col-md-6 posts">
+				Loading...
+			</div>
+			<div class="col-md-6">
+				<h2>Photos from Makercon</h2>
+				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+					<script async defer>
+						var images = [
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2498.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2514.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2562.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2578.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2592.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2605.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2613.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2622.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2649.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2678.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2743.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2769.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2793.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2812.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2895.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2919.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2926.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2933.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2936.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2946.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2957.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A2967.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A3004.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A3019.jpg"
+							},
+							{
+								"path": "http://makercon.com/wp-content/uploads/2014/05/MakerCon-2014-1H2A3030.jpg"
+							}
+						]
+
+					</script>
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+					</ol>
+
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner home-slideshow">
+					</div>
+
+					<!-- Controls -->
+					<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+					</a>
+					<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+					</a>
+				</div>
+
+			</div>
+		</div>
 		<div class="row">
 			<div class="col-md-12">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/big-themes.jpg" alt="Workshop Co-Chairs" class="">
