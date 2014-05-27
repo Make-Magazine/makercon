963ab449a96a0adb5ab0f8c1f9e21def4f8d6321
Jake Spurlock <jspurlock@makermedia.com>
2014-05-27 14:24:59 -0700

    Shuffle the posts output.

--- js/navigation.js
+++ js/navigation.js
@@ -33,19 +33,47 @@
 	};
 } )();
 
+function getRandomArbitrary( min, max, not ) {
+	digit = Math.floor( Math.random() * (max - min) + min );
+	// In theory, we could bring the same post back. This will grab another
+	// random value. This doesn't prevent that from going any farther, but
+	// hey, I don't think that anyone will notice/mind...
+	if ( digit == not ) {
+		digit = Math.floor( Math.random() * (max - min) + min );
+	};
+	return digit;
+}
+
 jQuery( document ).ready( function( $ ) {
-	var url = 'https://public-api.wordpress.com/rest/v1/sites/makezine.com/posts/?tag=makercon&number=2';
+	var url = 'https://public-api.wordpress.com/rest/v1/sites/makezine.com/posts/?tag=makercon&number=20';
 	var jqxhr = $.getJSON( url, function() {
+		// Posts...
+		var posts = jqxhr.responseJSON.posts;
+
+		// Start the output.
 		var html_output = '<h2>News from MakerCon</h2><ul class="recent-posts media-list">';
-		$( jqxhr.responseJSON.posts ).each( function() {
+
+		// Figure out how many posts we have to work with...
+		var length = jqxhr.responseJSON.posts.length;
+
+		// Setup the index array.
+		index = [];
+
+		// Get the first random number.
+		index.push( getRandomArbitrary( 0, length, null ) );
+		index.push( getRandomArbitrary( 0, length, index[0] ) );
+
+		// Loop through the new index.
+		$( index ).each( function( key, value ) {
 			html_output += '<li class="media">';
-			html_output += '<a class="pull-left" href="' + $(this)[0].URL + '"><img class="media-object thumbnail" src="' + $(this)[0].featured_image + '?w=125" alt="' + $(this)[0].title + '"></a>';
-			html_output += '<h3><a href="' + $(this)[0].URL + '">' + $(this)[0].title + '</a></h3>';
-			html_output += $(this)[0].excerpt;
+			html_output += '<a class="pull-left" href="' + posts[ value ].URL + '"><img class="media-object thumbnail" src="' + posts[ value ].featured_image + '?w=125" alt="' + posts[ value ].title + '"></a>';
+			html_output += '<h3><a href="' + posts[ value ].URL + '">' + posts[ value ].title + '</a></h3>';
+			html_output += posts[ value ].excerpt;
 			html_output += '</li>';
 		});
 		html_output += '</ul>';
 		$('.highlights .posts').html( html_output );
+
 	});
 
 	var slides = '';
