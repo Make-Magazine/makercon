c0461d3182e58118859c3eae60516f1c38a78e51
Bill Olson <bolson@makermedia.com>
2014-07-16 18:11:02 -0700

    Home page layout tweak

--- content-home.php
+++ content-home.php
@@ -151,6 +151,14 @@
 		<div class="row">
 			<div class="col-md-12">
 				<p><strong>MakerCon is a conference by and for makers.</strong> The maker movement is providing new insights into local and global manufacturing, design, workforce development, education and even creative culture. MakerCon provides valuable, practical insights into the impact of makers and making on education, the economy and emerging markets. </p>
+			<p><strong>What is MakerCon about?</strong>
+
+<p>MakerCon connects individuals at the forefront of the maker movement, focusing on the new technologies, services ecosystem, manufacturing models, and funding trends that provide new ways of making things and getting them to market.  Four overarching themes frame the discussion: education, community, technology and business.</p>
+
+<p>A two-day format, MakerCon includes epic keynotes, informal fireside chats, provocative panels and thematic meetups featuring experts in digital manufacturing, technology and tool innovators, leaders of higher education and the new breed of industrial designers.  These leaders come together to exchange their views and visions about the impact of the maker movement, and its sweeping measure beyond only new business and new technologies. </p>
+
+<p>Joining the conversation are partners and principals from top VC firms and crowdfunding services, and accelerators and incubators inventing new models to nurture the growing number of startups compelling the next generation of disruptive trends.  Rounding out the attendance will be makers across the spectrum: from passionate, young entrepreneurs testing the commercial waters with their prototypes, to those driving the creative culture and maker initiatives in their community, to product developers looking for inspiration from the maker movement. The hallmark will be the Innovation Showcase, spotlighting two dozen of the most fascinating makers and their inspired inventions.</p>
+
 			</div>
 		</div>
 		<div class="row" style="margin: 30px auto;">
@@ -163,14 +171,17 @@
 				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 			</div>
 			<div class="col-md-4">
-				<h2 style="text-align:left;">What is MakerCon about?</h2>
-
-<p>MakerCon connects individuals at the forefront of the maker movement, focusing on the new technologies, services ecosystem, manufacturing models, and funding trends that provide new ways of making things and getting them to market.  Four overarching themes frame the discussion: education, community, technology and business.</p>
-
-<p>A two-day format, MakerCon includes epic keynotes, informal fireside chats, provocative panels and thematic meetups featuring experts in digital manufacturing, technology and tool innovators, leaders of higher education and the new breed of industrial designers.  These leaders come together to exchange their views and visions about the impact of the maker movement, and its sweeping measure beyond only new business and new technologies.</p>
-
-<p>Joining the conversation are partners and principals from top VC firms and crowdfunding services, and accelerators and incubators inventing new models to nurture the growing number of startups compelling the next generation of disruptive trends.  Rounding out the attendance will be makers across the spectrum: from passionate, young entrepreneurs testing the commercial waters with their prototypes, to those driving the creative culture and maker initiatives in their community, to product developers looking for inspiration from the maker movement. The hallmark will be the Innovation Showcase, spotlighting two dozen of the most fascinating makers and their inspired inventions.</p>
-
+				<h2 style="text-align:left;">Our MakerCon mission and agenda:</h2>
+				<ul class="bullets">
+					<li>Celebrate makers and making</li>
+					<li>Recognize innovation and ingenuity</li>
+					<li>Meet and mix with makers</li>
+					<li>Exchange views and visions</li>
+					<li>Explore the potential of tools &amp; technology</li>
+					<li>Visualize the future (robots &amp; drones!)</li>
+					<li>Delve into emerging market trends </li>
+					<li>Demo exciting new products</li>
+				</ul>
 			</div>
 		</div>
 		<!-- <div class="row">
