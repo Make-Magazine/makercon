55f13e85c70f9e91b21046dc039abbabc79e774b
Bill Olson <bolson@makermedia.com>
2014-07-25 17:40:39 -0700

    Sponsors update with tracking code

--- sidebar.php
+++ sidebar.php
@@ -33,18 +33,18 @@
 			 <aside class="sponsors widget">
 				<h1 class="widget-title" style="margin-bottom:20px;">Our Sponsors</h1>
 				<h3>Presenting Sponsor</h3>
-				<a href="http://pubads.g.doubleclick.net/gampad/clk?id=146214418&iu=/11548178/Makezine">
+			<a href="http://pubads.g.doubleclick.net/gampad/clk?id=146214418&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
 					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cornell_logo-02.png" alt="Cornell Logo" width="250" style="margin-top:25px" /></a>
 				<h3 style="margin-top:20px;">Participating Sponsor</h3>
-		    	<a href="http://pubads.g.doubleclick.net/gampad/clk?id=146214658&iu=/11548178/Makezine">
+	    	              <a href="http://pubads.g.doubleclick.net/gampad/clk?id=146214658&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/fusion-autodesk-sponsor-250.png" alt="Autodesk Fusion Logo" /></a>
 
 					<h3 style="margin-top:20px;">Media Partner</h3>
-		    	<a href="http://pubads.g.doubleclick.net/gampad/clk?id=146214658&iu=/11548178/Makezine">
+		    	<a href="http://pubads.g.doubleclick.net/gampad/clk?id=147819658&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/engadget-logo-sidebar.gif" alt="Engadget Logo" width="200" /></a>
 
 					<h3 style="margin-top:20px;">Communications Partner</h3>
-		    	<a href="http://pubads.g.doubleclick.net/gampad/clk?id=146214658&iu=/11548178/Makezine">
+		    	<a href="http://pubads.g.doubleclick.net/gampad/clk?id=147820018&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/marketwired_logo.jpg" alt="Marketwired Logo" width="200" /></a>
 
 			</aside>
