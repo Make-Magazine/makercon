d97fca52a3daad1a9bf14fb324df29e7820c2507
Bill Olson <bolson@makermedia.com>
2014-08-06 16:54:43 -0700

    navigation and video page tweak

--- functions.php
+++ functions.php
@@ -85,10 +85,8 @@ function makercon_scripts() {
 	// wp_enqueue_style( 'makercon-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'makercon-bootstrap', get_stylesheet_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), false, true );
-
+	wp_enqueue_script( 'makercon-bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true );
 	wp_enqueue_script( 'makercon-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
 	wp_enqueue_script( 'makercon-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
