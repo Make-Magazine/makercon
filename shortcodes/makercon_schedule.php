f310255f6aec16bdb08bb6714dda7f9b7cac6640
Bill Olson <bolson@makermedia.com>
2014-08-14 10:57:52 -0700

    tracks tweaks

--- shortcodes/makercon_schedule.php
+++ shortcodes/makercon_schedule.php
@@ -15,20 +15,22 @@ function makercon_schedule_shortcode($atts){
 		'orderby' => 'title', 'order' => 'ASC', 'posts_per_page' => -1 ));
 
 	ob_start();
-	?>		
+	?>
 	<div class="sessions-list-area">
-	<?php 
-		
+	<?php
+
 		foreach($wp_sessions_posts as $session_post) {
 			setup_postdata($session_post); ?>
+			<div class="row">
+				<div class="track-info">
+					<p>PARTICULAR TRACK DESCRIPTION HERE (dynamic)</p>
+				</div>
+			</div>
 		<div class="row sessions-wrap">
 		  <div class="col-md-12 session">
 		    <h2 class="session-title"><a href="<?php echo(get_post_permalink($session_post->ID)); ?>">
 				<?php echo(esc_html($session_post->post_title)); ?></a></h2>
 		    <p class="session-author"><a href="SESSION AUTHOR LINK">SESSION AUTHOR NAME</a></p>
-		    <div class="button track-btn">
-		      <a href="TRACK TAG LINK">TRACK TAG</a> 
-		    </div>
 		  </div>
 		</div> <?php } ?>
 	</div><?php
