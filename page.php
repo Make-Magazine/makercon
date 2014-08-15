f4754713e0f48b53969bdf126cf306fd6ab86c54
Dan P. Gailey <dan@techendo.co>
2014-08-15 03:19:52 -0700

    speakers code

--- page.php
+++ page.php
@@ -12,7 +12,7 @@
 
 get_header(); ?>
 <div class="row">
-  <div id="primary" class="content-area col-md-8">
+  <div id="primary" class="content-area col-md-12">
     <main id="main" class="site-main" role="main">
 
       <?php while ( have_posts() ) : the_post(); ?>
