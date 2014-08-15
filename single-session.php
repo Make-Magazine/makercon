7a8e3d3479075bf426e7e6045ffa7bb724927efb
John <neurofog@gmail.com>
2014-08-15 14:03:14 -0700

    Formatted Loop in Session page

--- single-session.php
+++ single-session.php
@@ -7,7 +7,7 @@
 
 get_header(); 
 
-global $_show_speaker_more_link;
+global $_show_speaker_website_link;
 ?>
 
 <div class="row">
@@ -19,7 +19,7 @@ global $_show_speaker_more_link;
       $wp_post_speaker_ids = get_post_meta(get_the_ID(), '_session_speakers', true);
       if($wp_post_speaker_ids != '') {
         echo ("<h2>Speakers</h2>");
-        $_show_speaker_more_link = true;
+        $_show_speaker_website_link = true;
       get_template_part('loop','speaker-list');
       } ?>
 
