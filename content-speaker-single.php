10423c094c79d657711eaa7162977e60db02bebd
John <neurofog@gmail.com>
2014-08-14 23:16:30 -0700

    Add sub-title to Speaker page

--- content-speaker-single.php
+++ content-speaker-single.php
@@ -12,7 +12,11 @@
 			} ?></div>
 
 			<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-			<div class="lead"><h2><?php the_title(); ?></h2></div>
+			<div class="lead"><h2><?php the_title(); ?></h2><?php
+			$speaker_subtitle = get_post_meta(get_the_ID(), '_speaker_subtitle', true );
+			if($speaker_subtitle != '') {
+				print("<h3 class=\"speaker-subtitle\">".esc_html($speaker_subtitle)."</h3>");
+			} ?></div>
 			<?php the_content(); ?>
 			</div>
 		</div>		
