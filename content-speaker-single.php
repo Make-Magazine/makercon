5919e586d4870d49e86700b87f067dbf8144d6ec
Joshua J Wright <josh@joshuajwright.com>
2014-08-19 15:45:10 -0500

    Fix to column sizes on speaker page

--- content-speaker-single.php
+++ content-speaker-single.php
@@ -5,12 +5,12 @@
 ?>
 <article id="speaker-<?php the_ID(); ?>" <?php post_class(); ?>>
 <div class="row">
-      <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <div class="col-xs-5 col-sm-4 col-md-3 col-lg-3">
       <?php 
       if(get_post_meta(get_the_ID(), '_thumbnail_id', true ) !== false) {
         echo get_the_post_thumbnail(get_the_ID(), 'small-thumbnail' );
       } ?></div>
-      <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+      <div class="col-xs-7 col-sm-8 col-md-9 col-lg-9">
       <div class="lead"><h2><?php the_title(); ?></h2><?php
       $speaker_subtitle = get_post_meta(get_the_ID(), '_speaker_subtitle', true );
       if($speaker_subtitle != '') {
