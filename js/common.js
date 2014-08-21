5affb6897fffeb521675a3d950edc1b886487bb8
Dan P. Gailey <dan@techendo.co>
2014-08-20 23:34:44 -0700

    debugging something on production

--- js/common.js
+++ js/common.js
@@ -3,12 +3,13 @@ $(function() {
     var url = document.location.toString();
     if ( url.match( '#' ) ) {
       var tab_name = '.nav-tabs a[href=#' + url.split( '#' )[1] + ']';
-      $(tab_name).tab( 'show' ) ;
+      console.log(tab_name);
+      $(tab_name).tab( 'show' );
     }
 
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-       // console.log('showing tab ' + e.target); // Active Tab
-       // console.log('showing tab ' + e.relatedTarget); // Previous Tab
+        console.log('showing tab ' + e.target); // Active Tab
+        console.log('showing tab ' + e.relatedTarget); // Previous Tab
 
         // Use the History API if possible, or else, fall back.
         // The History API will allow us to hash a URL without page jump in modern browsers.
