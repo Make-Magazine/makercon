b9abb5d0c91edd495c990f02df82aac636980592
Dan P. Gailey <dan@techendo.co>
2014-08-21 00:05:49 -0700

    debugging something on production

--- sessions-custom.php
+++ sessions-custom.php
@@ -7,40 +7,6 @@
  */
 
 get_header(); ?>
-<script type="text/javascript">
-jQuery(document).ready(function ($) {
-  jQuery.fn.exists = function(){return this.length>0;}
-
-  if($('.nav-tabs').exists()) {
-    if(window.location.hash) {
-      var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
-      $('.nav-tabs').children('li').each(function () {
-        if($(this).data('hash') == hash) {
-          $(this).addClass('active');
-        } else {
-          $(this).removeClass('active');
-        }
-
-      })
-
-      if($('.tab-content').exists()) {
-        $('.tab-content').children('div.tab-pane').each(function () {
-          if($(this).data('hash') == hash) {
-              $(this).addClass('active');
-          } else {
-              $(this).removeClass('active');
-          }
-        });
-    
-      }
-
-     
-     //alert (hash);
-    }
-  }
-
-});
-</script>
 <div class="row">
   <div id="primary" class="content-area col-md-12">
     <main id="main" class="site-main" role="main">
