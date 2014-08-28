014b7e5495fc1f95a352090d3f4bac762cade748
Bill Olson <bolson@makermedia.com>
2014-08-28 10:18:40 -0700

    Restoring newer rocket logo

--- header.php
+++ header.php
@@ -69,7 +69,7 @@
             <div class="container">
 
         <div class="col-md-4 col-lg-4 hidden-sm hidden-xs text-center">
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-rocket.png" class="rocket img-responsive">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-rocket-sponsor.png" class="rocket img-responsive">
         </div>
         <div class="col-md-8 text-center slider">
                     <?=do_shortcode('[home-slider]');?>
