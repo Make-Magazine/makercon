4cf3052ee762f468eb49ef9c2c0bd97eec28c2bb
Bill Olson <bolson@makermedia.com>
2014-09-17 08:13:32 -0700

    video feed update

--- content-home.php
+++ content-home.php
@@ -17,7 +17,7 @@
       <h2 class="subtitle">Live from MakerCon New York 2014</h2>
       <div class="home-video-01">
         
-   <?php echo do_shortcode('[youtube=https://www.youtube.com/watch?v=/QcUWgQZ8W9c]');?>
+   <?php echo do_shortcode('[youtube=https://www.youtube.com/watch?v=QcUWgQZ8W9c]');?>
 </div>
 
         <div style="clear:both;"></div>
