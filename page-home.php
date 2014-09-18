c056a05f278e2461b748985f7e2569d9ad24b82d
John <neurofog@gmail.com>
2014-09-18 11:24:57 -0700

    Twitter Box height 550

--- page-home.php
+++ page-home.php
@@ -90,7 +90,7 @@ get_header(); ?>
         </div>
       </div>
       <div class="col-md-4">
-          <a class="twitter-timeline" href="https://twitter.com/search?q=%23makercon" data-widget-id="466630807038066688" height="450">Tweets about "#makercon"</a>
+          <a class="twitter-timeline" href="https://twitter.com/search?q=%23makercon" data-widget-id="466630807038066688" height="550">Tweets about "#makercon"</a>
           <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     </div>
     <!-- <div class="row">
