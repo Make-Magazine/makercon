54182b8f9a52a19c38e9b7d9b464c296d82b599a
Bill Olson <bolson@makermedia.com>
2014-10-03 17:14:14 -0700

    secondary nav changes

--- page-speakers.php
+++ page-speakers.php
@@ -12,23 +12,31 @@
 
 get_header(); ?>
 <div class="row">
-               <div class="col-md-12" style="padding-left:0px;padding-right:0px;">
-               <ul class="nav nav-tabs">
-                     <li class="col-md-3 tab">
+   <div class="col-md-12">
+      <div class="top-navbar">
+         <div class="navbar">
+            <div class="containter">
+               <ul class="nav nav-tabs nav-justified">
+                  <li>
+                     <h2 class="subtitle"><a href="/new-york-2014/conference/">OVERVIEW</a></h2>
+                  </li>
+                  <li>
                      <h2 class="subtitle"><a href="/new-york-2014/videos/">VIDEOS</a></h2>
                   </li>
-                  <li class="active col-md-3 tab">
+                  <li class="active">
                      <h2 class="subtitle"><a href="#">SPEAKERS</a></h2>
                   </li>
-                     <li class="col-md-3 tab">
+                  <li>
                      <h2 class="subtitle"><a href="/new-york-2014/sessions/">SESSIONS</a></h2>
                   </li>
-                  <li class="col-md-3 tab">
+                  <li>
                      <h2 class="subtitle"><a href="/new-york-2014/slides/" target="_blank">SLIDESHARE</a></h2>
                   </li>
-
                </ul>
             </div>
+         </div>
+      </div>
+   </div>
 </div>
 <div class="row">
   <div id="primary" class="content-area col-md-12">
--- page-videos-NY2014.php
+++ page-videos-NY2014.php
@@ -8,24 +8,33 @@
     *
     */
    get_header(); ?>
-   <div class="row">
-               <div class="col-md-12" style="padding-left:0px;padding-right:0px;">
-               <ul class="nav nav-tabs">
-                     <li class="active col-md-3 tab">
+<div class="row">
+   <div class="col-md-12">
+      <div class="top-navbar">
+         <div class="navbar">
+            <div class="containter">
+               <ul class="nav nav-tabs nav-justified">
+                  <li>
+                     <h2 class="subtitle"><a href="#">OVERVIEW</a></h2>
+                  </li>
+                  <li class="active">
                      <h2 class="subtitle"><a href="#">VIDEOS</a></h2>
                   </li>
-                  <li class="col-md-3 tab">
+                  <li>
                      <h2 class="subtitle"><a href="/new-york-2014/speakers-2">SPEAKERS</a></h2>
                   </li>
-                     <li class="col-md-3 tab">
+                  <li>
                      <h2 class="subtitle"><a href="/new-york-2014/sessions/">SESSIONS</a></h2>
                   </li>
-                  <li class="col-md-3 tab">
+                  <li>
                      <h2 class="subtitle"><a href="/new-york-2014/slides/" target="_blank">SLIDESHARE</a></h2>
                   </li>
                </ul>
             </div>
+         </div>
+      </div>
    </div>
+</div>
 <div class="row">
    <div id="primary" class="content-area col-md-12">
       <main id="main" class="site-main" role="main">
@@ -1064,8 +1073,4 @@
    }); 
      
 </script>
-
-
 <?php get_footer(); ?>
-
-
