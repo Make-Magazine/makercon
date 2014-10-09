172a5e23faea196275a59da42f2c7fdf572815c9
Bill Olson <bolson@makermedia.com>
2014-10-09 10:39:11 -0700

    consolidating javascript

--- functions.php
+++ functions.php
@@ -384,3 +384,28 @@ function make_modal_builder( $atts, $content = null ) {
 }
 add_shortcode( 'modal', 'make_modal_builder' );
 
+// Removing coment-reply .js
+
+function clean_header(){
+  wp_deregister_script( 'comment-reply' );
+         }
+add_action('init','clean_header');
+
+// Remove Jetpack CSS
+function remove_jetpack_styles(){
+wp_deregister_style('AtD_style'); // After the Deadline
+wp_deregister_style('jetpack-carousel'); // Carousel
+wp_deregister_style('grunion.css'); // Grunion contact form
+wp_deregister_style('the-neverending-homepage'); // Infinite Scroll
+wp_deregister_style('infinity-twentyten'); // Infinite Scroll - Twentyten Theme
+wp_deregister_style('infinity-twentyeleven'); // Infinite Scroll - Twentyeleven Theme
+wp_deregister_style('infinity-twentytwelve'); // Infinite Scroll - Twentytwelve Theme
+wp_deregister_style('noticons'); // Notes
+wp_deregister_style('post-by-email'); // Post by Email
+wp_deregister_style('publicize'); // Publicize
+wp_deregister_style('sharedaddy'); // Sharedaddy
+wp_deregister_style('sharing'); // Sharedaddy Sharing
+wp_deregister_style('stats_reports_css'); // Stats
+wp_deregister_style('jetpack-widgets'); // Widgets
+}
+add_action('wp_print_styles', 'remove_jetpack_styles');
