f015bcdf242413092c58941c51264732680d224b
Bill Olson <bolson@makermedia.com>
2014-10-09 10:45:08 -0700

    removing call for js files

--- functions.php
+++ functions.php
@@ -92,8 +92,6 @@ function makercon_scripts() {
   // wp_enqueue_style( 'makercon-style', get_stylesheet_uri() );
 
   wp_enqueue_script( 'jquery' );
-  wp_enqueue_script( 'makercon-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-  wp_enqueue_script( 'makercon-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
   wp_enqueue_script( 'common-files', get_template_directory_uri() . '/js/common.js', array(), '20140820-1', true );
 
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
