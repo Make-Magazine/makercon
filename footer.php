fb51ef165728561a0469500d77056bc50c08cb3d
Bill Olson <bolson@makermedia.com>
2015-01-23 15:49:30 -0800

    sponsor update

--- footer.php
+++ footer.php
@@ -20,40 +20,9 @@
    <div class="col-md-3">
       <h3 class="subtitle">
       Host Sponsor</h2>
-      <a href="http://pubads.g.doubleclick.net/gampad/clk?id=151334578&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
-      <img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/img/NYSCI_Logo_200w.gif" alt="NYSCI Logo" width="150" />
-      </a>
-   </div>
-   <div class="col-md-6">
-      <h3 class="subtitle">
-      Participating Sponsors</h2>
-      <div class="row">
-         <div class="col-md-6">
-            <a href="http://pubads.g.doubleclick.net/gampad/clk?id=146214658&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
-            <img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/img/fusion-autodesk-sponsor-398.png" alt="Autodesk Logo" width="300" style="width:300px;margin-top:30px;"   />
-            </a>
-         </div>
-         <div class="col-md-6">
-            <a href="http://pubads.g.doubleclick.net/gampad/clk?id=150606898&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
+      <a href="http://pubads.g.doubleclick.net/gampad/clk?id=150606898&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
             <img style="margin-top:25px;" src="<?php echo get_stylesheet_directory_uri(); ?>/img/intel_logo.jpg" alt="Intel Logo" width="100"  />
-            </a>
-         </div>
-      </div>
-</div>
-</div>
-<div class="row sponsors" style="text-align:center; border-bottom:1px solid #999;padding-bottom:30px !important;margin-bottom:60px;margin-top:30px;">
-   <div class="col-md-6">
-      <h3 class="subtitle">
-      Media Partner</h2>
-      <a href="http://pubads.g.doubleclick.net/gampad/clk?id=147819658&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
-      <img style="margin:auto;" class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/img/engadget-logo-sidebar.gif" alt="Engadget Logo" width="200" />
       </a>
-   </div>
-   <div class="col-md-6">
-      <h3 class="subtitle">
-      Communications Partner</h2>
-      <a href="http://pubads.g.doubleclick.net/gampad/clk?id=147820018&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
-      <img style="margin:auto;" class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/img/marketwired_logo.jpg" alt="Marketwired Logo" width="150" /></a>
    </div>
 </div>
 <?php endif; ?>
--- page-home.php
+++ page-home.php
@@ -42,19 +42,10 @@
     </div>
   </div>
   <div class="row sponsors-home">
-    <div class="col-md-6">
+    <div class="col-md-12">
       <h2 class="subtitle">Presenting Sponsor</h2>
-      <a href="http://pubads.g.doubleclick.net/gampad/clk?id=146214418&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
-      <img class="cornell-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/cornell_logo-02.png" alt="Cornell Logo" width="300" />
-      </a>
-    </div>
-    <div class="col-md-6">
-      <h2 class="subtitle">Participating Sponsors</h2>
-      <a href="http://pubads.g.doubleclick.net/gampad/clk?id=146214658&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
-      <img class="autodesk-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/fusion-autodesk-sponsor-398.png" alt="Autodesk Logo" width="300" />
-      </a>
       <a href="http://pubads.g.doubleclick.net/gampad/clk?id=150606898&iu=/11548178/Makezine" onClick="_gaq.push(['_trackEvent', 'SponsorAds', 'Click', 'AdLogo']);">
-      <img class="intel-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/intel_logo.jpg" alt="Intel Logo" width="100" />
+      <img class="" src="<?php echo get_stylesheet_directory_uri(); ?>/img/intel_logo.jpg" alt="Intel Logo" width="100" align="center" style="text-align:center;" />
       </a>
     </div>
   </div>
