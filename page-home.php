7bd23e9080d995f0a0b9751e82cd110f311e30f2
Bill Olson <bolson@makermedia.com>
2015-02-23 12:21:30 -0800

    Home page video swap

--- page-home.php
+++ page-home.php
@@ -26,10 +26,10 @@
         <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
         <?php endif; ?>
         <div class="hidden-xs hidden-sm col-md-3 col-lg-3 col-xl-3"></div>
-        <div class="button text-center col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <!-- <div class="button text-center col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
           <a href="/new-york-2014/videos/">More Videos From MakerCon New York 2014</a>
         </div>
-        <div class="hidden-xs hidden-sm col-md-3 col-lg-3 col-xl-3"></div>
+        <div class="hidden-xs hidden-sm col-md-3 col-lg-3 col-xl-3"></div> -->
       </div>
       <div class="col-md-4">
         <?php featured_speakers_function(); ?>
