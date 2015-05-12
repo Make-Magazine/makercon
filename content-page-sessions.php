8c95504d0e940caec38cf760aab60365bf230c57
David Beauchamp <dbeauchamp@makermedia.com>
2015-05-12 11:43:55 -0700

    Better schedule tabs on mobile.

--- content-page-sessions.php
+++ content-page-sessions.php
@@ -8,8 +8,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <header class="entry-header">
     <ul id="block-nav" class="nav nav-tabs">
-      <li class="active col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6"><a href="#day-one" data-toggle="tab">Day One</a></li>
-      <li class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6" style="background-color:##DBDBDB"><a href="#day-two" data-toggle="tab">Day Two</a></li>
+      <li class="active col-xs-6"><a href="#day-one" data-toggle="tab">Day One</a></li>
+      <li class="col-xs-6" style="background-color:##DBDBDB"><a href="#day-two" data-toggle="tab">Day Two</a></li>
       <!--li class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4"><a href="#showcase" data-toggle="tab">Innovation Showcase</a></li-->
     </ul>
   </header><!-- .entry-header -->
