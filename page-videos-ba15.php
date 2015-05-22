928c79ada5d0db9c46fb95d079e57bfe1e500582
Bill Olson <bolson@makermedia.com>
2015-05-21 17:14:09 -0700

    Video archive update

--- page-videos-ba15.php
+++ page-videos-ba15.php
@@ -8,38 +8,12 @@
     *
     */
    get_header(); ?>
-<div class="row sub-nav">
-   <div class="col-md-12">
-      <div class="top-navbar">
-         <div class="navbar">
-               <ul class="nav nav-tabs nav-justified">
-                  <li>
-                     <h2 class="subtitle"><a href="/bay-area-2015/conference/">OVERVIEW</a></h2>
-                  </li>
-                  <li class="active">
-                     <h2 class="subtitle"><a href="#">VIDEOS</a></h2>
-                  </li>
-                  <li>
-                     <h2 class="subtitle"><a href="/bay-area-2015/speakers">SPEAKERS</a></h2>
-                  </li>
-                  <li>
-                     <h2 class="subtitle"><a href="/bay-area-2015/sessions/#business">SESSIONS</a></h2>
-                  </li>
-                  <li>
-                    <h2 class="subtitle"><a href="/sessions/launch-pad-competition">LAUNCH PAD</a></h2>
-                  </li>
-               </ul>
-            </div>
-         </div>
-   </div>
-</div>
 <div class="row">
    <div id="primary" class="content-area col-md-12 video-archive">
       <main id="main" class="site-main" role="main">
          <div class="row">
             <div class="col-md-12">
                         <h1><?php echo get_the_title(); ?></h1>
-
          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
          <article <?php post_class(); ?>>
             <?php the_content(); ?>
@@ -49,34 +23,13 @@
          <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
          <?php endif; ?>
          <div class="clearfix">&nbsp;</div>
-             <!-- Button trigger modal -->
-        <!--        <img class="img-responsive" src="http://img.youtube.com/vi/WT4L42q2tgQ/maxresdefault.jpg" width="1170" data-toggle="modal" data-target="#myModal"/>
-               <div style="clear:both;"></div>
-               <p style="margin:10px auto 15px auto;text-align:center;font-size:18px;line-height:21px;">MakerCon Bay Area 2015</p>
-               --> <!-- Modal -->
- <!--               <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                  <div class="modal-dialog">
-                     <div class="modal-content">
-                        <div class="modal-header">
-                           <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                           <h4 class="modal-title" id="myModalLabel">MakerCon Bay Area 2015</h4>
-                        </div>
-                        <div class="modal-body">
-                           <iframe width="780" height="448" src="https://www.youtube.com/embed/WT4L42q2tgQ&amp;showinfo=0&amp;rel=0?html5=1&amp;showinfo=0&amp;rel=0?html5=1" frameborder="0" allowfullscreen></iframe>
-                        </div>
-                        <div class="modal-footer">
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div> -->
+
          </div>
          <div class="row">
             <div class="col-xs-12">
                <h2 class="subtitle">More Videos from MakerCon Bay Area 2015</h2>
             </div>
          </div>
-         
          <!-- <div class="row">
             <div class="col-md-12">
                <ul class="nav nav-tabs">
