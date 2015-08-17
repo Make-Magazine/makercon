7b251efd148b29c40ad53a5bf515710634a22502
David Beauchamp <dbeauchamp@makermedia.com>
2015-08-17 15:01:12 -0700

    Getting rid of some useless templates. Fixing the Schedule template.

--- page-custom-sessions.php
+++ page-custom-sessions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Custom Sessions Page
+ * Template Name: Schedule
  *
  * @package    makercon
  * @license    http://opensource.org/licenses/gpl-license.php  GNU Public License
@@ -9,31 +9,6 @@
  */
 
 get_header(); ?>
-<div class="row ny2014-nav">
-   <div class="col-md-12">
-      <div class="top-navbar">
-         <div class="navbar">
-               <ul class="nav nav-tabs nav-justified">
-                  <li>
-                     <h2 class="subtitle"><a href="/new-york-2014/conference/">OVERVIEW</a></h2>
-                  </li>
-                  <li>
-                     <h2 class="subtitle"><a href="/new-york-2014/videos/">VIDEOS</a></h2>
-                  </li>
-                  <li>
-                     <h2 class="subtitle"><a href="/new-york-2014/speakers">SPEAKERS</a></h2>
-                  </li>
-                  <li class="active">
-                     <h2 class="subtitle"><a href="#">SESSIONS</a></h2>
-                  </li>
-                  <li>
-                     <h2 class="subtitle"><a href="/new-york-2014/slides/" target="_blank">SLIDESHARE</a></h2>
-                  </li>
-               </ul>
-            </div>
-      </div>
-   </div>
-</div>
 <div class="row">
 	<div id="primary" class="content-area col-md-12">
 		<main id="main" class="site-main" role="main">
