736705c64a6bfff7a776970cc52af94e01891a33
David Beauchamp <dbeauchamp@makermedia.com>
2015-09-18 17:19:55 -0700

    Need to fix the GA tracking url for click tracking of the brand bar.

--- header.php
+++ header.php
@@ -206,10 +206,10 @@
           <div class="col-sm-3">
           </div>
           <div class="col-sm-6 text-center">
-            <p class="header-make-img"><a href="//makezine.com?utm_source=makershed.com&utm_medium=brand+bar&utm_campaign=explore+all+of+make" target="_blank">Explore all of <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/make_logo.png" alt="Make: Makezine Logo" /></a></p>
+            <p class="header-make-img"><a href="//makezine.com?utm_source=makercon.com&utm_medium=brand+bar&utm_campaign=explore+all+of+make" target="_blank">Explore all of <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/make_logo.png" alt="Make: Makezine Logo" /></a></p>
           </div>
           <div class="col-sm-3">
-            <p class="header-sub-link pull-right"><a href="//readerservices.makezine.com/mk?utm_source=makershed.com&utm_medium=brand+bar&utm_campaign=mag+sub" target="_blank">Subscribe </a></p>
+            <p class="header-sub-link pull-right"><a href="//readerservices.makezine.com/mk?utm_source=makercon.com&utm_medium=brand+bar&utm_campaign=mag+sub" target="_blank">Subscribe </a></p>
           </div>
         </div>
       </div>   
